735d72837e854cde31df1ceb13c539e00101d52f
Michał Łoza <scorneds@gmail.com>
2012-04-21 12:56:42 +0200

    d

---
+++ src/choroby/Objaw.java
@@ -0,0 +1,8 @@
+package choroby;
+
+import java.io.Serializable;
+
+
+public class Objaw implements Serializable {
+	protected String name = "Objaw";
+}
---
+++ src/files/CipherNotFoundException.java
@@ -0,0 +1,5 @@
+package files;
+
+public class CipherNotFoundException extends Exception {
+
+}
