ff2213d3180a7cf996581453633be9e344798109
Michał Łoza <scorneds@gmail.com>
2012-04-21 12:54:38 +0200

    Zapis dziaaaajcccccccccy

--- src/choroby/Choroba.java
+++ src/choroby/Choroba.java
@@ -1,15 +1,30 @@
 package choroby;
 
+import java.io.Externalizable;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 import files.*;
 
-public class Choroba implements Writable, Redable {
+public class Choroba implements Externalizable {
 	private String nazwa = "asd";
 	private String opis = "asd";
-	public String[] objawy;
+	protected List<Objaw> Objawy = new ArrayList<Objaw>();
 	private String[] fieldsToSave = {"nazwa", "opis"};
+	
+	public Choroba()
+	{
+		Objawy.add(new Objaw());
+	}
 	
 	public String[] fieldsToSave()
 	{
@@ -36,29 +51,69 @@ public class Choroba implements Writable, Redable {
 	{
 		System.out.println("start");
 		
-		/*FileOp.save(new Choroba());
+		//FileOp2.write(new Choroba(), "choroba.txt", "none");
+		try {
+			ObjectOutputStream op = new ObjectOutputStream(new FileOutputStream("data/choroba.txt"));
+			op.writeObject(new Choroba());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		Choroba testowa = new Choroba();
 		System.out.println("Przed ustawienim");
 		testowa.printValues();
 		testowa.setValues();
 		System.out.println("Po ustawienum");
 		testowa.printValues();
-		FileOp.read(testowa);
+		try {
+			ObjectInputStream op = new ObjectInputStream(new FileInputStream("data/choroba.txt"));
+			testowa = (Choroba)op.readObject();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		System.out.println("Po odczycie");
-		testowa.printValues();*/
+		testowa.printValues();
 		
 		List<Choroba> lista = new LinkedList<Choroba>();
 		lista.add(new Choroba());
 		Choroba ch = new Choroba();
 		ch.setValues();
 		lista.add(ch);
-		FileOp.save(lista, "choroby.txt");
+		//FileOp2.write(lista, "choroby.txt");
 		
-		List<Choroba> choroby = new LinkedList<Choroba>();
-		FileOp.read(choroby, "choroby.txt");
+		try {
+			ObjectOutputStream op = new ObjectOutputStream(new FileOutputStream("data/choroby.txt"));
+			op.writeObject(lista);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		List<Choroba> choroby = null;
+		try {
+			ObjectInputStream op = new ObjectInputStream(new FileInputStream("data/choroby.txt"));
+			choroby = (LinkedList<Choroba>)op.readObject();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		//FileOp2.read(choroby, "choroby.txt");
 		for(Choroba f : choroby)
 		{
 			f.printValues();
 		}
+		
+	}
+
+	public void writeExternal(ObjectOutput out) throws IOException {
+			out.writeObject(Objawy);
+			out.writeObject(nazwa);
+			out.writeObject(opis);
+	}
+
+	public void readExternal(ObjectInput in) throws IOException,
+			ClassNotFoundException {
+			Objawy = (ArrayList<Objaw>)in.readObject();
+			nazwa = (String)in.readObject();
+			opis = (String)in.readObject();
 	}
 }
